24f6320a23e8b1e72522462a63ebd3a48453b847
CodigoTotal01 <palaciostarrillochristian@gmail.com>
2023-03-02 22:24:52 -0500

    No corre por registro eureka inbestigar bug

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,11 +2,12 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
-@EnableFeignClients
+//@RibbonClient(name="servicio-productos")
+@EnableFeignClients // conectarce  para las api y consultas
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
