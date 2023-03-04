8af42fda3bcc34c65110b6640f23edcf1b151a44
CodigoTotal01 <palaciostarrillochristian@gmail.com>
2023-03-03 20:13:07 -0500

    Hystrix implementado

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -3,9 +3,11 @@ package com.formacionbdi.springboot.app.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@RibbonClient(name="servicio-productos")
+@EnableHystrix
 @EnableFeignClients // conectarce  para las api y consultas
 @EnableEurekaClient
 @SpringBootApplication
--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -2,6 +2,8 @@ package com.formacionbdi.springboot.app.item.controllers;
 
 import java.util.List;
 
+import com.formacionbdi.springboot.app.item.models.Producto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,19 +15,31 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 @RestController
 public class ItemController {
-	
-	@Autowired
-	@Qualifier("serviceFeign")
-	private ItemService itemService;
-	
-	@GetMapping("/listar")
-	public List<Item> listar(){
-		return itemService.findAll();
-	}
-	
-	@GetMapping("/ver/{id}/cantidad/{cantidad}")
-	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return itemService.findById(id, cantidad);
-	}
+
+    @Autowired
+    @Qualifier("serviceFeign")
+    private ItemService itemService;
+
+    @GetMapping("/listar")
+    public List<Item> listar() {
+        return itemService.findAll();
+    }
+
+    @HystrixCommand(fallbackMethod = "metodoAlternativo")
+    @GetMapping("/ver/{id}/cantidad/{cantidad}")
+    public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+        return itemService.findById(id, cantidad);
+    }
+    //! Este deveria disparar un nuevo vlasor json - codigo duro
+    public Item metodoAlternativo(Long id, Integer cantidad) {
+        Item item = new Item();
+        Producto producto = new Producto();
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("EL producto buscado no existe :D");
+        producto.setPrecio(500.00);
+        item.setProducto(producto);
+        return item;
+    }
 
 }
